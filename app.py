c2a54310218653d6a8473f77456bbd0c8233a3bc
Munir <dunepart2@moonear.ca>
2024-03-13 09:21:02 -0600

    good

--- app.py
+++ app.py
@@ -1,12 +1,11 @@
-txt = "abcaa"
 
-vowel_count = 0
-# count the vowels in some text
-# in abc, one vowel
-
-for char in txt:
-    if char in ["a", "e", "i", "o", "u"]:
-        # increment a counter
-        vowel_count += 1
-
-print(f"number of vowels is {vowel_count}")
+def count_vowels(txt):
+    vowel_count = 0
+    # count the vowels in some text
+    # in abc, one vowel
+    for char in txt:
+        if char in ["a", "e", "i", "o", "u"]:
+            # increment a counter
+            vowel_count += 1
+    return vowel_count
+print(f"number of vowels is {count_vowels("abcaa")}")
